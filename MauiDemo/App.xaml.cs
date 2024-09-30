7b6e56023095f94b0550e7778e9be8aad10aec3c
amandabtorres <banaszeskiamanda@gmail.com>
2024-09-30 21:10:38 +0100

    Layouts - AbsoluteLayout

--- MauiDemo/App.xaml.cs
+++ MauiDemo/App.xaml.cs
@@ -11,7 +11,7 @@
             navPage.BarBackground = Colors.Yellow;
             navPage.BarTextColor = Colors.Blue;
 
-            MainPage = new FlexLayoutDemo();
+            MainPage = new AbsoluteLayoutDemo();
         }
     }
 }
